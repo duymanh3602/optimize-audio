a533042c6412a0d8e6106d2f15883120104b7dea
duymanh3602 <82275607+duymanh3602@users.noreply.github.com>
2023-10-13 11:59:46 +0700

    fix: promise all

--- main.js
+++ main.js
@@ -3,22 +3,18 @@ import { fetchAudioData } from './fetchData.js';
 
 const args = process.argv.slice(2);
 const filePath = args[0];
+let promiseAll = [];
 
 readTxtFile(filePath).then((rows) => {
   if (rows) {
     const chunkSize = 10;
     for (let i = 0; i < rows.length; i += chunkSize) {
+      let promiseAll = [];
       const chunk = rows.slice(i, i + chunkSize);
-      fetchAudioData(chunk[0]);
-      fetchAudioData(chunk[1]);
-      fetchAudioData(chunk[2]);
-      fetchAudioData(chunk[3]);
-      fetchAudioData(chunk[4]);
-      fetchAudioData(chunk[5]);
-      fetchAudioData(chunk[6]);
-      fetchAudioData(chunk[7]);
-      fetchAudioData(chunk[8]);
-      fetchAudioData(chunk[9]);
+      for (let j = 0; j < chunk.length; j++) {
+        promiseAll.push(fetchAudioData(chunk[j]))
+      }
+      Promise.all(promiseAll);
     }
   }
 })
